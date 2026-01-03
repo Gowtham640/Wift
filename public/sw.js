8314b8ccaf247d057c27f9a3103c915cd61cd920
Gowtham640 <grizigowtham@gmail.com>
2026-01-03 11:57:02 +0530

    Mobile routing issue fix

--- public/sw.js
+++ public/sw.js
@@ -11,11 +11,3 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      caches.match('/').then((res) => res || fetch(event.request))
-    );
-  }
-});
